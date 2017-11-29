1523765d42f679ede88c2521c404e6d732aaef1e
XianMao <2132006423@qq.com>
2017-11-29 20:21:07 +0800

    改善了HP应用的模板页面

--- untitled/hp/views.py
+++ untitled/hp/views.py
@@ -15,10 +15,9 @@ def index(request):
 @permission_required('work_system.change_extra_work')
 def details(request, work_number):
     tempUser = User.objects.filter(username=work_number)
-    member_name = get_object_or_404(Personal.objects.filter(user = tempUser)).name
     member_hp = get_object_or_404(HP.objects.filter(user=tempUser))
     member_debuffs = DeBuff.objects.filter(key=member_hp)
     member_buffs = Buff.objects.filter(key=member_hp)
     member_all_buffs = list(chain(member_debuffs, member_buffs))
     member_all_buffs.sort(key=attrgetter('add_time'), reverse=True)
-    return render(request, 'hp/details.html', {'member_hp': member_hp, 'all_buffs': member_all_buffs, 'member_name':member_name})
+    return render(request, 'hp/details.html', {'member_hp': member_hp, 'all_buffs': member_all_buffs})
